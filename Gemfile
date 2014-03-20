cde29a7b587889c907d43b41f9df4e35dab7c218
zhang_yunjie <achillessatan@gmail.com>
2014-03-20 17:46:36 +0900

    memcached

--- Gemfile
+++ Gemfile
@@ -34,6 +34,10 @@ end
 
 gem 'setty', github: "AchillesSatan/setty"
 
+gem 'rack-cache'
+gem 'dalli'
+gem 'kgio'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-CrazyStudio::Application.config.session_store :cookie_store, key: '_crazy-studio_session'
+# CrazyStudio::Application.config.session_store :cookie_store, key: '_crazy-studio_session'
+
+params = {
+  key: "_crazy-studio_session_",
+  cookie_only: false,
+  timeout: 1.5,
+  memcache_server: Setty.base.session_memcache_server,
+  expire_after: 1.hour,
+  namespace: "crazy-studio-#{Rails.env}"
+}
+
+CrazyStudio::Application.config.session_store ActionDispatch::Session::CacheStore
+CrazyStudio::Application.config.session_options = params
