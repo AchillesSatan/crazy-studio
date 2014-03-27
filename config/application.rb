7cdb8c45940319771a461dff2055bdc7c9f215c0
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-03-27 17:38:30 +0900

    config禁止

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module CrazyStudio
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # とりあえず  
+    config.action_controller.allow_forgery_protection = false
+
     config.cache_store = :dalli_store, Setty.base.cache_memcache_server, {namespace: "crazy-studio-#{Rails.env}"}
 
     config.action_mailer.default_url_options = { :host => 'localhost:3000' }
