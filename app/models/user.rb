a941562dfb9e59fa866d15771fca23838ac47c6f
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-03-27 17:29:05 +0900

    action->filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  skip_before_action :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
 
   include User::BlogModule
 
