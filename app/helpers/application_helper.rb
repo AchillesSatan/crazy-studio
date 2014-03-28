bcd1277930df5c7d82e829ec8b3bc3190510b370
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-03-28 15:43:46 +0900

    local gravastar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   require "digest/md5"
   def gravatar(email, size = 48)
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    return "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{asset_path("ninja-avatar-48x48.png")}"
+    return "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=/public#{asset_path("ninja-avatar-48x48.png")}"
   end
 end
