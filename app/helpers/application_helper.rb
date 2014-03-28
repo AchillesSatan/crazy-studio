fda4ebe983e97a7552ca141d1570f01b29359c7e
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-03-28 15:22:45 +0900

    navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  require "digest/md5"
+  def gravatar(email, size = 48)
+    gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    return "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=https://dashboard.heroku.com/ninja-avatar-48x48.png"
+  end
 end
