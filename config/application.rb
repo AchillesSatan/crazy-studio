59c8cf6d61f0fdecc7b43eade492a46ef6649ff9
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-03-28 11:13:59 +0900

    try

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module CrazyStudio
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    config.time_zone = 'Tokyo'
+    # config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
