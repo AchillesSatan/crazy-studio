d175d81499e4eba0f2cb684db5815f3eb214c1e4
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-03-31 16:59:41 +0900

    blog index layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   require "digest/md5"
-  def gravatar(email, size = 48)
+  def gravatar(email, size = 48, default_url = "&d=https://dashboard.heroku.com/ninja-avatar-48x48.png")
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
-    return "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=https://dashboard.heroku.com/ninja-avatar-48x48.png"
+    return "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}#{default_url}"
   end
 end
