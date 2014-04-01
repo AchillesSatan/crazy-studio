6d0a2bedbe2fa5a08ff1ca7692cd53dfc0a87393
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-04-01 19:37:34 +0900

    tab

--- app/admin/blog.rb
+++ app/admin/blog.rb
@@ -10,10 +10,6 @@ ActiveAdmin.register Blog do
   end
 
   filter :title
-
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # permit_params :list, :of, :attributes, :on, :model
   #
--- app/models/user_blog.rb
+++ app/models/user_blog.rb
@@ -1,4 +1,4 @@
 class UserBlog < ActiveRecord::Base
   belongs_to :user
-  belongs_to :blog
+  has_one :blog
 end
