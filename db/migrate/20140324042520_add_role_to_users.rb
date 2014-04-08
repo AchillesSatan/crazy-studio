d42906fbb397c9d58637231b1f93faebdd2e58fb
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-04-08 11:40:11 +0900

    db

--- db/migrate/20140324042520_add_role_to_users.rb
+++ db/migrate/20140324042520_add_role_to_users.rb
@@ -1,11 +1,13 @@
 class AddRoleToUsers < ActiveRecord::Migration
   def change
     add_column :users, :role, :string, :default => 'reader', after: :email
+    add_column :users, :name, :string, after: :role
 
     User.create! do |u|
-      u.email    = 'admin@example.com'
+      u.email    = 'achillessatan@gmail.com'
       u.password = 'admin123'
       u.role     = 'administrator'
+      u.name     = 'yunjie.zhang'
     end
   end
 end
