3c076aeeff49c3994e48d9a103041b8230c01909
zhang_yunjie <yunjie.zhang@drecom.co.jp>
2014-04-09 14:14:57 +0900

    boog#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,20 @@
 module ApplicationHelper
+
   require "digest/md5"
+
   def gravatar(email, size = 48, default_url = "&d=https://dashboard.heroku.com/ninja-avatar-48x48.png")
     gravatar_id = Digest::MD5.hexdigest(email.downcase)
     return "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}#{default_url}"
+  end
+
+  def safe_render(options = {}, locals = {}, &block)
+    begin
+      render options, locals, &block
+    rescue => exception
+      Raven.capture_exception(exception)
+      logger.info exception.inspect
+      logger.info exception.backtrace.grep(/^((?!\/gems\/).)*$/).join("\n")
+      nil
+    end
   end
 end
